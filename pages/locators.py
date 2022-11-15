ab3e19fe2e067938ba1bc06b83a21b26f4950508
KarpovAlexey95 <karpov2695@gmail.com>
2022-11-15 17:18:52 +0300

    add test product page

--- pages/locators.py
+++ pages/locators.py
@@ -5,6 +5,15 @@ class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
-class LoginPageLocators ():
+class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    ADD_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
+    ADD_ALERT_PRODUCT_NAME = (By.CLASS_NAME, "alertinner strong")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div h1")
+    BASKET_ALERT = (By.CSS_SELECTOR, ".alertinner p strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
+
---
+++ pages/product_page.py
@@ -0,0 +1,30 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        add_button = self.browser.find_element(*ProductPageLocators.ADD_BUTTON)
+        add_button.click()
+        assert True
+
+    def should_be_add_button(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_BUTTON), "Add button is not presented"
+
+    def should_be_add_alert(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_ALERT_PRODUCT_NAME), "Add alert is not presented"
+
+    def check_add_alert(self):
+        add_alert = self.browser.find_element(*ProductPageLocators.ADD_ALERT_PRODUCT_NAME)
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
+        assert product_name.text == add_alert.text, "Incorrect product name in add alert"
+
+    def should_be_basket_alert(self):
+        assert self.is_element_present(*ProductPageLocators.BASKET_ALERT), "Basket alert is not presented"
+
+    def check_basket_alert(self):
+        add_alert = self.browser.find_element(*ProductPageLocators.BASKET_ALERT)
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
+        basket = add_alert.text.split()[0]
+        price = product_name.text.split()[0]
+        assert basket == price, "Incorrect price in basket alert"
