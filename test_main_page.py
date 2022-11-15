16361c88254093fa6993c5908b9b60e230011387
KarpovAlexey95 <karpov2695@gmail.com>
2022-11-15 14:51:39 +0300

    add exception handling

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,3 @@
-from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
 
 
@@ -9,9 +8,11 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
 
 
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
 # pytest -v --tb=line --language=en test_main_page.py
 # def test_add_to_cart(browser):
